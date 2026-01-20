18e828c530bdab3662a135886b7af16edebd7e83
Naomi Nzioka <nziokanaomi422@gmail.com>
2026-01-20 15:07:00 +0300

    Fetch fox image on mount using useEffect

--- src/components/FoxImage.jsx
+++ src/components/FoxImage.jsx
@@ -5,7 +5,18 @@ const API_URL = "https://randomfox.ca/floof/"
 
 function FoxImage() {
   const [image, setImage] = useState(foxLogo)
-  
+    useEffect(() => {
+    // fetch here 
+    fetch(API_URL)
+         .then(response => {
+        if (!r.ok) { throw new Error("Failed to fetch image"); }    
+        return response.json();
+      })
+      .then(data => setImage(data.image))
+      .catch(error => console.log(error));
+  }, []);
+
+
   return (
     <div>
       <p>Learn more about us!</p>
